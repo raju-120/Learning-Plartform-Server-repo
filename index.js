884d680a897a7fb0c3f13d4b51870e2cc430969b
Raju <ashfaqraju27@gmail.com>
2022-11-10 15:35:38 +0600

    data added

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const port = process.env.PORT || 5000;
 
 
 app.use(cors());
-
 const courses = require ('./Data/coursesItem.json');
+const details = require ('./Data/details.json');
+
 
 app.get('/', (req, res) =>{
     res.send('Learning Platform server is running');
@@ -14,6 +15,12 @@ app.get('/', (req, res) =>{
 
 app.get('/courses', (req, res) =>{
     res.send(courses);
+})
+
+app.get('/details/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedCourse = details.find( d => d._id === id);
+    res.send(selectedCourse);    
 })
 
 app.listen(port, () => {
