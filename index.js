8de28fd43b536fcdb04b7ccac32be4d7e313e118
Raju <ashfaqraju27@gmail.com>
2022-11-14 18:19:56 +0600

    data uploaded

--- index.js
+++ index.js
@@ -5,29 +5,32 @@ const port = process.env.PORT || 5000;
 
 
 app.use(cors());
-const courses = require ('./Data/coursesItem.json');
-const details = require ('./Data/details.json');
+const category = require ('./Data/category.json');
+const courses = require ('./Data/course.json');
 
 
 app.get('/', (req, res) =>{
     res.send('Learning Platform server is running');
 });
 
-app.get('/courses', (req, res) =>{
-    res.send(courses);
+app.get('/course-category', (req, res) =>{
+    res.send(category);
+});
+
+app.get('/category/:id', (req, res) =>{
+    const id = req.params.id;
+    const category_course = courses.filter( c => c.category_id === id);
+    res.send(category_course); 
 })
 
-app.get('/courses/:id', (req, res) =>{
+app.get('/course/:id', (req, res) =>{
     const id = req.params.id;
-    const courses_details = cDtails.filter( c => c.courseItem_id === id);
-    res.send(courses_details);
-})
+    const selected = courses.find( c=> c._id === id);
+    res.send(selected);
+});
 
-app.get('/details/:id', (req, res) =>{
-    const id = req.params.id;
-    const selectedCourse = details.find( d => d.courseItem_id === id);
-    res.send(selectedCourse);    
-})
+
+
 
 app.listen(port, () => {
     console.log('Learning platform Server running on port', port);
